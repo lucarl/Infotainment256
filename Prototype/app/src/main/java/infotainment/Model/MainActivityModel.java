e567297dc19dc2376f2be5da4b45bd2c3ef16bfa
Sigbjörn Drösshaug <q3defrag@gmail.com>
2018-10-10 13:58:42 +0200

    clr implement
    getters and setters in MainActivityModel interface

--- Prototype/app/src/main/java/infotainment/Model/MainActivityModel.java
+++ Prototype/app/src/main/java/infotainment/Model/MainActivityModel.java
@@ -1,14 +1,40 @@
 package infotainment.Model;
 
+import android.graphics.Color;
+
 import infotainment.contract.MainActivityContract;
 
 public class MainActivityModel implements MainActivityContract.Model{
 
+    private int startClr;
+    private int endClr;
 
+    public MainActivityModel() {
+        startClr = Color.GREEN;
+        endClr = Color.RED;
+    }
     @Override
     public String getData() {
 
         return "Starting";
     }
 
+    @Override
+    public int getStartColor() {
+        return startClr;
+    }
+    @Override
+    public int getEndColor() {
+        return endClr;
+    }
+
+    @Override
+    public void setStartColor(int newStartClr) {
+        startClr = newStartClr;
+    }
+
+    @Override
+    public void setEndColor(int newEndClr) {
+        endClr = newEndClr;
+    }
 }
