68e91d8d23a2b15988ca941dfb777a9adf8cf14c
lucarl <carl.e.w.lundborg@gmail.com>
2018-10-11 13:36:56 +0200

    small change.

---
+++ Prototype/app/src/main/java/infotainment/presenter/StatisticsPresenter.java
@@ -0,0 +1,14 @@
+package infotainment.presenter;
+
+import android.view.View;
+
+import infotainment.contract.MainActivityContract;
+
+public class StatisticsPresenter implements MainActivityContract.Presenter{
+
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
