97057fc8635f376b928d44d57144c1106a7369d1
lucarl <carl.e.w.lundborg@gmail.com>
2018-10-11 14:28:39 +0200

    statistics

---
+++ Prototype/app/src/main/java/infotainment/contract/StatisticsActivityContract.java
@@ -0,0 +1,23 @@
+package infotainment.contract;
+
+public interface StatisticsActivityContract {
+
+    interface View {
+
+        void initView();
+        void setViewData(String data);
+
+    }
+
+    interface Model {
+
+        String getData();
+    }
+
+    interface Presenter {
+
+        void onClick(android.view.View view);
+
+    }
+
+}
