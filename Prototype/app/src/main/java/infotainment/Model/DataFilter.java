2c107e5bb38833fb7ae7358d4c021ad139f337ea
ThomasFL <frodin@student.chalmers.se>
2018-10-12 11:05:59 +0200

    sync before merge

--- Prototype/app/src/main/java/infotainment/Model/DataFilter.java
+++ Prototype/app/src/main/java/infotainment/Model/DataFilter.java
@@ -1,7 +1,11 @@
 package infotainment.Model;
 
+import android.renderscript.Sampler;
+import android.util.Pair;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 public class DataFilter {
 
@@ -10,11 +14,35 @@ public class DataFilter {
 
 
     public DataFilter() {
+        // Short term list
         SamplingList = new ArrayList<>(); //Arrays.asList(1,2,3,4,5)
+        // Long term list
         AverageList = new ArrayList<>();
+    }
+
+    public int getRand(){
+        Random rand = new Random();
+        return rand.nextInt(50);
+    }
+
+    // interface function for other programs to call to input data.
+    public void dataInput(Character c, int inData) {
+        if (AverageList.size() >= 10) {
+            // send average to database
+            // external.function(AvgDouble(AverageList));
+            db.insert(new Pair<Character, Double>(c, AvgDouble(AverageList)));
+            AverageList.clear();
+        }
+        if (SamplingList.size() >= 10){
+            AverageList.add(AvgInt(SamplingList));
+            SamplingList.clear();
+        }
+        SamplingList.add(inData);
+
 
     }
-    public Double AvgDouble(ArrayList<Double> intArr) {
+    // Gets average value off array of Doubles
+    private Double AvgDouble(ArrayList<Double> intArr) {
         Double sum = 0.0;
         if(!intArr.isEmpty()) {
             for(Double elem : intArr) {
@@ -25,7 +53,8 @@ public class DataFilter {
         return sum;
 
     }
-    public Double AvgInt(ArrayList<Integer> intArr) {
+    // Gets average value off array of Integers
+    private Double AvgInt(ArrayList<Integer> intArr) {
         Integer sum = 0;
         if(!intArr.isEmpty()) {
             for(int elem : intArr) {
@@ -51,6 +80,8 @@ public class DataFilter {
     fill AverageList until 10th element
 
     perform average on AverageList and output to db
+
+    timer object to inject values via data input method
 
     */
 
